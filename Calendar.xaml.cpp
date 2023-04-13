b63d75cee8b96112040ea6d6a9990cf7670a0e2f
Misha Osipov <mexx7557@gmail.com>
2023-04-13 21:18:09 +0300

    Calendar

---
+++ Calendar.xaml.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "Calendar.xaml.h"
+#if __has_include("Calendar.g.cpp")
+#include "Calendar.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::DairyApp::implementation
+{
+    Calendar::Calendar()
+    {
+        InitializeComponent();
+    }
+
+    int32_t Calendar::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void Calendar::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+}
---
+++ Calendar.xaml.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "Calendar.g.h"
+
+namespace winrt::DairyApp::implementation
+{
+    struct Calendar : CalendarT<Calendar>
+    {
+        Calendar();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::DairyApp::factory_implementation
+{
+    struct Calendar : CalendarT<Calendar, implementation::Calendar>
+    {
+    };
+}
