a1c4480f26a3efd51922d82bd09999e3d98fedef
Dmitry <dimamikheyev04@gmail.com>
2023-04-22 01:52:35 +0300

    Add some task interface

--- TaskPage.xaml.cpp
+++ TaskPage.xaml.cpp
@@ -1,12 +1,17 @@
 ﻿#include "pch.h"
 #include "TaskPage.xaml.h"
 #include "TaskClass.h"
+#include "TaskPage.xaml.g.h"
 #if __has_include("TaskPage.g.cpp")
 #include "TaskPage.g.cpp"
 #endif
 
+#include <vector>
+#include <string>
+
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
+using namespace winrt::DairyApp::implementation;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,4 +31,23 @@ namespace winrt::DairyApp::implementation
     {
         throw hresult_not_implemented();
     }
+}
+
+std::vector <std::string> UserTask;
+
+void TaskPage::AppBarButton_Click(Windows::Foundation::IInspectable const& sender, RoutedEventArgs const& e)
+{
+    //Txt2().Text(to_hstring(""));
+    std::string s = to_string(Txt().Text());
+    UserTask.push_back(s);
+
+    std::string s1 = "";
+
+    for (int i = 0; i < UserTask.size(); i++)
+    {
+        s1 += UserTask[i];
+        s1 += ' ';
+    }
+
+    Txt2().Text(to_hstring(s1));
 }
--- TaskPage.xaml.h
+++ TaskPage.xaml.h
@@ -10,6 +10,7 @@ namespace winrt::DairyApp::implementation
 
         int32_t MyProperty();
         void MyProperty(int32_t value);
+        void AppBarButton_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
     };
 }
 
