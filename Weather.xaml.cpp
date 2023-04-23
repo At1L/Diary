156db5c7c591c1b25fc0418f1d18f4aa2d1c38a2
Misha Osipov <mexx7557@gmail.com>
2023-04-23 23:58:36 +0300

    Add user page and weather page

---
+++ Weather.xaml.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "Weather.xaml.h"
+#if __has_include("Weather.g.cpp")
+#include "Weather.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::DairyApp::implementation
+{
+    Weather::Weather()
+    {
+        InitializeComponent();
+    }
+
+    int32_t Weather::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void Weather::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+}
---
+++ Weather.xaml.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "Weather.g.h"
+
+namespace winrt::DairyApp::implementation
+{
+    struct Weather : WeatherT<Weather>
+    {
+        Weather();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::DairyApp::factory_implementation
+{
+    struct Weather : WeatherT<Weather, implementation::Weather>
+    {
+    };
+}
