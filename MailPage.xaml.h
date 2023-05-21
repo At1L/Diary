2522a038153eb929a914ffc235dd16d2a609f542
Misha Osipov <mexx7557@gmail.com>
2023-05-22 00:21:18 +0300

    Update User page and add Mail page

---
+++ MailPage.xaml.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "MailPage.g.h"
+
+namespace winrt::DairyApp::implementation
+{
+    struct MailPage : MailPageT<MailPage>
+    {
+        MailPage();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        
+    };
+}
+
+namespace winrt::DairyApp::factory_implementation
+{
+    struct MailPage : MailPageT<MailPage, implementation::MailPage>
+    {
+    };
+}
